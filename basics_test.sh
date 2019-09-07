63cec06603c85e53e9419d5f64c039312c6668da
Sayter99 <ing5698yi@hotmail.com>
2019-09-07 14:51:53 -0600

    Fix get_column_stats.py 1. [add] argparse 2. [add] comments 3. [add] exception handling and exit 4. [improve] basics_test

--- basics_test.sh
+++ basics_test.sh
@@ -1,17 +1,40 @@
-pycodestyle style.py
+#!/bin/bash
 
-pycodestyle get_column_stats.py
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run style_test_style pycodestyle style.py
+assert_no_stdout
+run style_test_get_column_stats pycodestyle get_column_stats.py
+assert_no_stdout
 
 (for i in `seq 1 100`; do 
     echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
 
-python get_column_stats.py data.txt 2
-
+run basic_stats python get_column_stats.py --file_name data.txt --column_number 2
+assert_exit_code 0
+run invalid_column_text python get_column_stats.py --file_name data.txt --column_number abc
+assert_exit_code 1
+run invalid_column python get_column_stats.py --file_name data.txt --column_number 10
+assert_exit_code 1
 
 V=1
 (for i in `seq 1 100`; do 
     echo -e "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
 
-python get_column_stats.py data.txt 2
+echo "...running get_column_stats.py with data.txt and 2..."
+run basic_stats python get_column_stats.py --file_name data.txt --column_number 2
+assert_exit_code 0
+echo "...show help of get_column_stats.py..."
+run help_command python get_column_stats.py -h
+assert_exit_code 0
+
+(for i in `seq 1 100`; do 
+    echo -e "$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM";
+done )> data.txt
+
+echo "...test get_column_stats.py with invalid file..."
+run bad_file python get_column_stats.py --file_name data.txt --column_number 3
+assert_exit_code 1
--- get_column_stats.py
+++ get_column_stats.py
@@ -1,19 +1,90 @@
-import sys, math
+"""Get Column Stats
+    this program will show both mean and stdev of a column of the input file
+    Parameters
+    ----------
+    --file_name : the name of the file
+    --column_number : the target column
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
+    Returns
+    -------
+    mean : mean of the column
+    stdev: standard deviation of the column
+"""
+import sys
+import math
+import argparse
 
-f = open(file_name, 'r')
 
-V = []
+# parse arguments (file_name, column_number)
+def parse_args():
+    parser = argparse.ArgumentParser(
+                description='The right way to pass parameters.',
+                prog='get_column_stats')
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+    # require file name as one of the inputs
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='Name of the file',
+                        required=True)
 
-mean = sum(V)/len(V)
+    # require column number as one of the inputs
+    parser.add_argument('--column_number',
+                        type=str,
+                        help='The column number',
+                        required=True)
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    return parser.parse_args()
 
-print('mean:', mean)
-print('stdev:', stdev)
+
+if __name__ == '__main__':
+    # get argument by arg parser
+    args = parse_args()
+    # verify the input col_num is an integer
+    col_num = None
+    try:
+        col_num = int(args.column_number)
+    except ValueError:
+        print('Please enter an integer')
+        sys.exit(1)
+
+    # check whether the file is valid
+    file_name = args.file_name
+    f = None
+    try:
+        f = open(file_name, 'r')
+    except FileNotFoundError:
+        print('Could not find ' + file_name)
+        sys.exit(1)
+    except PermissionError:
+        print('Could not open ' + file_name)
+        sys.exit(1)
+
+    V = []
+
+    # fill numbers of the input column into V
+    for l in f:
+        # check the file format
+        try:
+            A = [int(x) for x in l.split()]
+        except ValueError:
+            print('Bad format of the input file')
+            sys.exit(1)
+        # check the column number is valid or not
+        try:
+            V.append(A[col_num])
+        except IndexError:
+            print('Invalid Column Number')
+            sys.exit(1)
+
+    # deal with empty V and calculate (mean, stdev)
+    if (len(V) != 0):
+        mean = sum(V)/len(V)
+        stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    else:
+        mean = 0
+        stdev = 0
+
+    # show results
+    print('mean:', mean)
+    print('stdev:', stdev)
+    sys.exit()
